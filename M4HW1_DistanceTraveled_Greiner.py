514ba1c759831c67dc98c7c51a9e3b3d79d19d41
greinerb3996 <33226353+greinerb3996@users.noreply.github.com>
2017-11-15 11:11:45 -0500

    Add files via upload

---
+++ M4HW1_DistanceTraveled_Greiner.py
@@ -0,0 +1,22 @@
+# CTI-110 
+# M4HW1 - Distance Traveled  
+# Brett Greiner
+# 11-15-17
+#This program calculates the rate and distance of certain problems
+print('Rate and time calculations')
+speed = 70
+distanceAfter6 = speed * 6
+distanceAfter10 = speed * 10
+distanceAfter15 = speed * 15
+#This calculates the distance for 6 hours of travel time
+print('At a speed of 70mph and drive time of 6 hours')
+print('Distance:', (distanceAfter6))
+#This calculates the distance for 10 hours of travel time
+print('At a speed of 70mph and drive time of 10 hours')
+print('Distance:', (distanceAfter10))
+#This caluclates the distance for 15 hours of travel time
+print('At a speed of 70mph and drive time of 15 hours')
+print('Distance:', (distanceAfter15))
+
+                    
+
