beb974612ca59c9d5ecac63759fbef7662a3aa6d
greinerb3996 <33226353+greinerb3996@users.noreply.github.com>
2017-11-26 23:28:25 -0500

    Add files via upload

---
+++ M5LAB_Greiner.py
@@ -0,0 +1,25 @@
+# I was supposed to put a comment here
+# My Last Name
+
+# This program takes a number grade and outputs a letter grade.
+
+# system uses 10-point grading scale
+
+def main():
+    A_score >= 90
+    B_score >= 80 and <= 89
+    C_score >= 70 and <= 79
+    D_score >= 60 and <= 69
+    F_score <= 59
+score = input('Enter score: '):
+    if score = A_score:
+        print('Your grade is: A')
+    if score = B_score:
+        print('Your grade is: B')
+    if score = C_score:
+        print('Your grade is: C')
+    if score = D_score:
+        print('Your grade is: D')
+    if score = F_score:
+        print('Your grade is: F')
+main()
