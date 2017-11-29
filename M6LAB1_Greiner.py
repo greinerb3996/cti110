0f1abeb454a0efe2e2df586d764c2d8d5dcfc217
greinerb3996 <33226353+greinerb3996@users.noreply.github.com>
2017-11-29 11:18:23 -0500

    Add files via upload

---
+++ M6LAB1_Greiner.py
@@ -0,0 +1,14 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> for a in [0,1,2,3]:
+	turtle.forward(60)
+	turtle.left(90)
+
+	
+>>> for b in [0,1,2]:
+	turtle.forward(90)
+	turtle.left(120)
+
+	
+>>> 
