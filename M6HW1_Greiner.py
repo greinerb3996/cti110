a1d9e49c0d90fedb6df7c67b2831afd76d71846a
greinerb3996 <33226353+greinerb3996@users.noreply.github.com>
2017-12-03 21:44:21 -0500

    Add files via upload

---
+++ M6HW1_Greiner.py
@@ -0,0 +1,26 @@
+# CTI-110 
+# M6HW2 - Distance Traveled  
+# Brett Greiner
+# 12/03/17
+
+def main():
+    print ('Distance Traveled')
+speed = int(input('What is the speed of the vehicle in mph?: '))
+time = int(input('How many hours has it traveled?: '))
+main()
+print('Distance traveled:', (speed * time))
+
+speed = int(input('What is the speed of the vehicle in mph?: '))
+time = int(input('How many hours has it traveled?: '))
+print('Distance traveled:', (speed * time))
+
+speed = int(input('What is the speed of the vehicle in mph?: '))
+time = int(input('How many hours has it traveled?: '))
+print('Distance traveled:', (speed * time))
+
+
+
+
+
+
+
