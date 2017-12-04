cb4c5b9f5743f5c79d9c84d65931ce3d9d725079
greinerb3996 <33226353+greinerb3996@users.noreply.github.com>
2017-12-04 11:42:55 -0500

    Add files via upload

---
+++ M7T2_Greiner.py
@@ -0,0 +1,18 @@
+# CTI 110
+# M7T2 Feet to Inches 
+# Brett Greiner
+# 12/04/2017
+
+#Constant for the number of inches per foot.
+INCHES_PER_FOOT = 12
+#Main function
+def main():
+    #Get a number of feet from the user.
+    feet = int(input('Enter a number of feet: '))
+    #Convert to inches.
+    print(feet, 'equals', feet_to_inches(feet), 'inches.')
+# The feet_to_inches function converts feet to inches.
+def feet_to_inches(feet):
+    return feet * INCHES_PER_FOOT
+# Call the main function.
+main()
