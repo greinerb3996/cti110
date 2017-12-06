e8bfc887f32175ff65a70fa0fbb4ab587ae23db8
greinerb3996 <33226353+greinerb3996@users.noreply.github.com>
2017-12-06 10:25:18 -0500

    Add files via upload

---
+++ M6T1A_Greiner.py
@@ -0,0 +1,15 @@
+# CTI-110 
+# M6T1A - Turtle Lab 
+# Brett Greiner 
+# 12/06/2017
+#
+
+import turtle
+
+for a in [0,1,2,3]:
+    turtle.forward(60)
+    turtle.left(90)
+
+for b in [0,1,2]:
+    turtle.forward(90)
+    turtle.left(120)
